d770e9008899b0d7c71e8cd7da01bf4a220bbb35
lihwang <lihwang_wf@126.com>
2024-08-02 17:08:29 +0800

    目录初始化

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
-
 <template>
   <div>
     <h1>Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
   </div>
+  <app-alert>222222</app-alert>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
